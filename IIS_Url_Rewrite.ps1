d60e428704b7fdf44f9337da6e824e627d367cc8
chaduvur <chaduvur@users.noreply.github.com>
2020-04-21 16:53:56 +0530

    IIS url rewrite rules

---
+++ IIS_Url_Rewrite.ps1
@@ -0,0 +1,21 @@
+﻿param
+(
+ [Parameter(Mandatory=$True,Position=0)]
+ [string]$SiteName,
+ [Parameter(Mandatory=$True,Position=1)]
+ [string]$AppName,
+ [Parameter(Mandatory=$True,Position=2)]
+ [string]$Port
+)
+
+$site = "iis:\sites\"
+$site="$site$SiteName"
+$rulename="ReverseProxyInboundRule_"
+$rulename = "$rulename$AppName"
+$filterRoot = "system.webServer/rewrite/rules/rule[@name='$rulename']"
+Clear-WebConfiguration -pspath $site -filter $filterRoot
+Add-WebConfigurationProperty -pspath $site -filter "system.webServer/rewrite/rules" -name "." -value @{name='$rulename';patternSyntax='Regular Expressions';stopProcessing='True'}
+Set-WebConfigurationProperty -pspath $site -filter "$filterRoot/match" -name "url" -value "$AppName(.*)"
+Set-WebConfigurationProperty -pspath $site -filter "$filterRoot/conditions" -name "{CACHE_URL}" -value "^(https?)://"
+Set-WebConfigurationProperty -pspath $site -filter "$filterRoot/action" -name "type" -value "Rewrite"
+Set-WebConfigurationProperty -pspath $site -filter "$filterRoot/action" -name "url" -value "{C:1}://localhost:$Port/{R:1}"
---
+++ IIS_Url_Rewrite_Latest.ps1
@@ -0,0 +1,29 @@
+﻿param
+(
+ [Parameter(Mandatory=$True,Position=0)]
+ [string]$SiteName,
+ [Parameter(Mandatory=$True,Position=1)]
+ [string]$AppName,
+ [Parameter(Mandatory=$True,Position=2)]
+ [string]$Port
+)
+
+$site = "IIS:\Sites\"
+$site="$site$SiteName"
+
+$name = "ReverseProxyInboundRule_"
+$name = "$name$AppName"
+
+$inbound = "{0}(.*)" -f $AppName
+$outbound = "{C:1}://localhost:$Port/{R:1}"
+$pattern = '^(https?)://'
+$root = 'system.webServer/rewrite/rules'
+$filter = "{0}/rule[@name='{1}']" -f $root, $name
+
+Clear-WebConfiguration -pspath $site -filter $filter
+Add-WebConfigurationProperty -PSPath $site -filter $root -name '.' -value @{name=$name; patterSyntax='Regular Expressions'; stopProcessing='True'}
+Set-WebConfigurationProperty -PSPath $site -filter "$filter/match" -name 'url' -value $inbound
+Set-WebConfigurationProperty -PSPath $site -filter "$filter/conditions" -name '.' -value @{input='{CACHE_URL}'; pattern=$pattern}
+Set-WebConfigurationProperty -PSPath $site -filter "$filter/action" -name 'type' -value 'Rewrite'
+Set-WebConfigurationProperty -PSPath $site -filter "$filter/action" -name 'url' -value $outbound
+Set-WebConfigurationProperty -PSPath $site -filter "$filter/action" -name 'logRewrittenUrl' -value 'True'
